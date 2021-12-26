eb9b5ffe13e533e6f95d6c4d3b055544c5488e3e
Aaryan Dewan <aaryandewan@yahoo.com>
2021-12-26 15:08:26 +0530

    Add News functionality

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,26 @@
+//services is the new Redux way of fetching data
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "x-bingapis-sdk": "true",
+  "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+  "x-rapidapi-key": "4bce4b6e83msh0d3fde274cc01c5p1cb050jsn38590baa04eb",
+};
+
+const baseURL = "https://bing-news-search1.p.rapidapi.com/";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl: baseURL }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
